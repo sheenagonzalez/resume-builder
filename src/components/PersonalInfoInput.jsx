8c3e5f4def1b021434061643d175eee49ebddca0
Sheena Gonzalez <shngonzalez@gmail.com>
2024-11-07 00:09:36 -0800

    Fix proptypes import

--- src/components/PersonalInfoInput.jsx
+++ src/components/PersonalInfoInput.jsx
@@ -1,4 +1,4 @@
-import { PropTypes } from "prop-types";
+import PropTypes from 'prop-types';
 function PersonalInfoInput({ fname, lname, jobTitle, email, phone, onChange }) {
   return (
     <section className="input-section">
--- src/components/ProjectsInput.jsx
+++ src/components/ProjectsInput.jsx
@@ -1,4 +1,4 @@
-import { PropTypes } from "prop-types";
+import PropTypes from 'prop-types';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import LinksInput from './LinksInput.jsx'
